1f4127a6bf7ce84bc4657449a68866ca4a74c4e1
floriankausche <floriankausche@byui.edu>
2024-06-15 21:02:24 -0400

    week one

---
+++ scripts/getdates.js
@@ -0,0 +1,19 @@
+// getdates.js
+
+// Function to set the current year
+function setCurrentYear() {
+    const currentYearSpan = document.getElementById('currentyear');
+    const currentYear = new Date().getFullYear();
+    currentYearSpan.textContent = currentYear;
+}
+
+// Function to set the last modified date
+function setLastModified() {
+    const lastModifiedParagraph = document.getElementById('lastModified');
+    const lastModifiedDate = new Date(document.lastModified);
+    lastModifiedParagraph.textContent = 'Last Modification: ' + lastModifiedDate.toLocaleString();
+}
+
+// Call the functions to set the dates
+setCurrentYear();
+setLastModified();
