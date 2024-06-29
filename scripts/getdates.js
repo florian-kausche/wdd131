3fe460b26f92db167cfc8db063817a9fd25bcd54
floriankausche <floriankausche@byui.edu>
2024-06-29 02:30:22 -0400

    week one

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,21 +1,19 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    // Function to set the current year
+    function setCurrentYear() {
+        const currentYearSpan = document.getElementById('currentyear');
+        const currentYear = new Date().getFullYear();
+        currentYearSpan.textContent = currentYear;
+    }
 
+    // Function to set the last modified date
+    function setLastModified() {
+        const lastModifiedParagraph = document.getElementById('lastModified');
+        const lastModifiedDate = new Date(document.lastModified);
+        lastModifiedParagraph.textContent = 'Last Modification: ' + lastModifiedDate.toLocaleString();
+    }
 
-// Function to set the current year
-function setCurrentYear() {
-    const currentYearSpan = document.getElementById('currentyear');
-    const currentYear = new Date().getFullYear();
-    currentYearSpan.textContent = currentYear;
-}
-
-// Function to set the last modified date
-function setLastModified() {
-    const lastModifiedParagraph = document.getElementById('lastModified');
-    const lastModifiedDate = new Date(document.lastModified);
-    lastModifiedParagraph.textContent = 'Last Modification: ' + lastModifiedDate.toLocaleString();
-}
-
-// Call the functions to set the dates
-setCurrentYear();
-setLastModified();
-
-//This code is not working online for me why?
+    // Call the functions to set the dates
+    setCurrentYear();
+    setLastModified();
+   });
