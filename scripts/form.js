24953f1f54c08502f46874c7a4242888b9641d62
floriankausche <floriankausche@byui.edu>
2024-07-14 18:40:39 -0400

    week five

--- scripts/form.js
+++ scripts/form.js
@@ -63,3 +63,10 @@ document.addEventListener("DOMContentLoaded", function() {
 
   
 });
+document.addEventListener('DOMContentLoaded', function () {
+    const yearSpan = document.getElementById('currentyear');
+    yearSpan.textContent = new Date().getFullYear();
+
+    const lastModifiedSpan = document.getElementById('lastModified');
+    lastModifiedSpan.textContent = 'Last Modified: ' + document.lastModified;
+});
