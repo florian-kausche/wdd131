0f261aa8938881a8f2d7de37ddbaa75711a63b31
floriankausche <floriankausche@byui.edu>
2024-07-14 16:44:17 -0400

    week five

--- scripts/form.js
+++ scripts/form.js
@@ -54,25 +54,12 @@ document.addEventListener("DOMContentLoaded", (event) => {
     setCurrentYear();
     setLastModified();
 });
+
 document.addEventListener("DOMContentLoaded", function() {
     let reviewCount = localStorage.getItem("reviewCount") || 0;
     reviewCount++;
     localStorage.setItem("reviewCount", reviewCount);
     document.getElementById("review-count").textContent = reviewCount;
-
-    // Function to set the current year
-    function setCurrentYear() {
-        const currentYearSpan = document.getElementById('currentyear');
-        const currentYear = new Date().getFullYear();
-        currentYearSpan.textContent = currentYear;
-    }
-
-    // Function to set the last modified date
-    function setLastModified() {
-        const lastModifiedParagraph = document.getElementById('lastModified');
-        const lastModifiedDate = new Date(document.lastModified);
-        lastModifiedParagraph.textContent = 'Last Modification: ' + lastModifiedDate.toLocaleString();
-    }
 
   
 });
