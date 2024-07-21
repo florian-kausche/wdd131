0a9506a4817b25b9848edc402e5112f3d231c2ac
floriankausche <floriankausche@byui.edu>
2024-07-21 18:50:02 -0400

    final project week 6

---
+++ final-project/scripts/scripts.js
@@ -0,0 +1,9 @@
+document.getElementById('menu').addEventListener('click', function() {
+    this.classList.toggle('open');
+    document.querySelector('nav').classList.toggle('open');
+});
+
+document.getElementById('menu').addEventListener('click', function() {
+    this.classList.toggle('open');
+    document.querySelector('.nav-links').classList.toggle('open');
+});
