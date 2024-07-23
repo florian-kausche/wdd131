12080585e6c349b90ff870993b8c1ef46e129506
floriankausche <floriankausche@byui.edu>
2024-07-23 18:17:03 -0400

    final project week 6

--- final-project/scripts/scripts.js
+++ final-project/scripts/scripts.js
@@ -1,13 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
     const menuButton = document.getElementById('menu');
-    const navLinks = document.querySelector('.nav-links');
+    const navLinks = document.querySelector('header nav ul');
 
     menuButton.addEventListener('click', function () {
+        // Toggle the 'open' class on both the menu button and the navigation links
         navLinks.classList.toggle('open');
         menuButton.classList.toggle('open');
     });
 
     window.addEventListener('resize', function () {
+        // If the window width is greater than 768px, make sure the menu is closed
         if (window.innerWidth > 768) {
             navLinks.classList.remove('open');
             menuButton.classList.remove('open');
