54c033b2befcad0d7a7f8487825957bf64618439
floriankausche <floriankausche@byui.edu>
2024-07-24 17:22:52 -0400

    final project week 6

--- final-project/scripts/loader.js
+++ final-project/scripts/loader.js
@@ -1,8 +1,8 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', () => {
     const menuButton = document.getElementById('menu');
     const navLinks = document.querySelector('.nav-links');
 
-    menuButton.addEventListener('click', function () {
+    menuButton.addEventListener('click', () => {
         navLinks.classList.toggle('open');
         menuButton.classList.toggle('open');
     });
